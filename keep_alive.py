8213fceafd8ba7e92853e217dd9f113686cc2b91
Ahosun Yousuf <92607011+AHOSUN-YOUSUF@users.noreply.github.com>
2024-02-22 14:48:12 +0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "`@Gemini Pro Multi-Modal` is online!"
+    return "`@Gemini Pro Multi-Modal#0747` is online!"
 
 def run():
     app.run(host = "0.0.0.0", port = 8080)
